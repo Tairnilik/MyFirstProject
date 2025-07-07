a5627c2a26981f80448b6828c1345e996776f497
TairKrytoi2025 <aldabergenoff.tair@yandex.ru>
2025-07-07 13:12:53 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿//MyFirstProject
+using System;
+
+string[] Analyze = { "Bob", "KDL", "Weak" };
+foreach (string Total in Analyze)
+{
+    Console.WriteLine($"INFORMATION : {Total}");
+}
+
+Console.WriteLine($"You are sick!");
